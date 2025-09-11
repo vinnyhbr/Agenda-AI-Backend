d172b28cc7927fda207596328b84f31b5c40f41c
Vinícius Rosa <viniciusrosalima14@gmail.com>
2025-09-11 09:40:11 -0300

    fix: ajustar versão Java para 17 e corrigir compatibilidade JWT

--- src/test/java/com/vini/agendaai/controller/AuthControllerTest.java
+++ src/test/java/com/vini/agendaai/controller/AuthControllerTest.java
@@ -8,9 +8,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
@@ -28,7 +28,7 @@ class AuthControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private UserService userService;
 
     @Autowired
